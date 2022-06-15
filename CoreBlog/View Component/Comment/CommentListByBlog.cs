dc256fc4f21baee9e83d023c92c8ee3965e27cef
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-06-15 23:39:41 +0300

    Blog yorumlarının listelenmesi

---
+++ CoreBlog/View Component/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.View_Component.Comment
+{
+    public class CommentListByBlog:ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.ListAll(4);
+            return View(values);
+        }
+    }
+}
