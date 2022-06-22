b55d1413cc17b1e704c742477bbc01083a2407c9
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-06-22 18:54:46 +0300

    dashboard duzenleme

---
+++ CoreBlog/View Component/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.View_Component.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var result = bm.GetBlogListWithCategory();
+            return View(result);
+        }
+    }
+}
---
+++ CoreBlog/View Component/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.View_Component.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var result = cm.TGetList();
+            return View(result);
+        }
+    }
+}
