47968466b6058cc16019f31b178349cf591176fe
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-06-24 18:06:13 +0300

    AddImage

--- CoreBlog/Controllers/WriterController.cs
+++ CoreBlog/Controllers/WriterController.cs
@@ -1,7 +1,13 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using CoreBlog.Models;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 namespace CoreBlog.Controllers
 {
@@ -25,12 +31,65 @@ namespace CoreBlog.Controllers
         {
             return PartialView();
         }
+        [HttpGet]
         [AllowAnonymous]
         public IActionResult WriterEditProfile()
 
         {
             var values = wm.TGetById(1);
             return View(values);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult WriterEditProfile(Writer writer)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult result = wl.Validate(writer);
+            if (result.IsValid)
+            {
+                wm.TUpdate(writer);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult WriterAdd(AddProfileImage writer)
+        {
+            Writer w = new Writer();
+            if (writer.WriterImage != null)
+            {
+                var extension = Path.GetExtension(writer.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                writer.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterMail = writer.WriterMail;
+            w.WriterName = writer.WriterName;
+            w.WriterPassword = writer.WriterPassword;
+            w.WriterAbout = writer.WriterAbout;
+            w.WriterStatus = true;
+
+
+            wm.TAdd(w);
+            return RedirectToAction("index", "Dashboard");
         }
     }
 }
