784f0e7be19eacdc96e3898fbceb50a1e68478d6
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-07-19 14:33:39 +0300

    Pagination

--- CoreBlog/Areas/Admin/Controllers/CategoryController.cs
+++ CoreBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,16 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
-
+using X.PagedList;
 namespace CoreBlog.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         [Area("Admin")]
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values=cm.TGetList();
+            var values=cm.TGetList().ToPagedList(page,3);
             return View(values);
         }
     }
