578c6aef6614bd5a7719d78c645eda13ff83c68e
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-07-19 13:27:23 +0300

    Kategori Listeleme

---
+++ CoreBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            var values=cm.TGetList();
+            return View(values);
+        }
+    }
+}
--- CoreBlog/Startup.cs
+++ CoreBlog/Startup.cs
@@ -83,6 +83,10 @@ namespace CoreBlog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                     name: "areas",
+                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+         );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
