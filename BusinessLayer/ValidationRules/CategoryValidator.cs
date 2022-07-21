65313309cce8687e9e22dced840e7370d5efc7ad
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-07-21 16:43:10 +0300

    Admine paneline kategori ekleme

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x=>x.CategoryName).NotEmpty().WithMessage("Kategori adı boş bırakılamaz");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklama kısmı boş bırakılamaz");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("Kategori adı 50 karakterden fazla olamaz");
+            RuleFor(x => x.CategoryName).MinimumLength(2).WithMessage("Kategori adını 2 karakterden fazla giriniz");
+        }
+    }
+}
--- CoreBlog/Areas/Admin/Controllers/CategoryController.cs
+++ CoreBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 namespace CoreBlog.Areas.Admin.Controllers
@@ -12,6 +15,33 @@ namespace CoreBlog.Areas.Admin.Controllers
         {
             var values=cm.TGetList().ToPagedList(page,3);
             return View(values);
+        }
+        [Area("Admin")]
+        [HttpGet]
+        public IActionResult CategoryAdd()
+        {
+            return View();  
+        }
+        [Area("Admin")]
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            CategoryValidator cv=new CategoryValidator();   
+            ValidationResult result=cv.Validate(p);
+            if (result.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index","Category");   
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
