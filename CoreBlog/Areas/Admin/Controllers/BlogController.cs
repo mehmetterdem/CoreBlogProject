6af819e1f73ab3d11ad9ff2a60b97a4e25955da6
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-07-25 15:16:42 +0300

    dynamic export excel

--- CoreBlog/Areas/Admin/Controllers/BlogController.cs
+++ CoreBlog/Areas/Admin/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
 ﻿using ClosedXML.Excel;
 using CoreBlog.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CoreBlog.Areas.Admin.Controllers
 {
@@ -45,6 +47,49 @@ namespace CoreBlog.Areas.Admin.Controllers
         }
         [Area("Admin")]
         public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        [Area("Admin")]
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using(var workbook=new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Dynamic Çalısma1");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Name";
+                int BlogRowCount = 2;
+
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.Id;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream(BlogRowCount))
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformatsofficedocuments.spreadsheetml.sheet", "DynamicCalısma1.xlsx");
+                }
+            }
+        }
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using(var c=new Context())
+            {
+               bm= c.Blogs.Select(x => new BlogModel2
+                {
+                    BlogName = x.BlogTitle,
+                    Id=x.BlogId,
+                }).ToList();
+            }
+            return bm;
+        }
+        [Area("Admin")]
+        public IActionResult BlogTitleListExcel()
         {
             return View();
         }
---
+++ CoreBlog/Areas/Admin/Models/BlogModel2.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreBlog.Areas.Admin.Models
+{
+    public class BlogModel2
+    {
+        public int Id { get; set; }
+        public string BlogName{ get; set; }
+    }
+}
