bac9a9cb0d11ec2afe32211acf62d057db7d3691
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-07-25 16:40:00 +0300

    istatistik

---
+++ CoreBlog/Areas/Admin/Controllers/WidgetController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreBlog.Areas.Admin.Controllers
+{
+    public class WidgetController : Controller
+    {
+
+        Context c = new Context();
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+         [Area("Admin")]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = bm.TGetList().Count();
+            ViewBag.v2=c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ CoreBlog/Areas/ViewComponents/Statistic1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Areas.ViewComponents
+{
+    public class Statistic1:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
