4c361642d880ca3480599f5b3f638e623b72b875
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-08-16 14:48:10 +0300

    identity ile login

--- CoreBlog/Controllers/LoginController.cs
+++ CoreBlog/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using DataAccessLayer.Concrete;
+﻿using CoreBlog.Models;
+using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,52 +13,81 @@ using System.Threading.Tasks;
 
 namespace CoreBlog.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        [AllowAnonymous]
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(Writer writer)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            #region Claims
-            Context c = new Context();
-            var result = c.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
-            if (result != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>()
+                var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, false, true);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name,writer.WriterMail)
-                };
-                var useridentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("index", "dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("index", "login");
+                }
             }
-            else
-            {
-                return View();
-            }
-
-            #endregion
-            #region Session
-            //Context c = new Context();
-            //var result = c.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
-            //if (result != null)
-            //{
-            //    HttpContext.Session.SetString("username", writer.WriterMail);
-            //    return RedirectToAction("Index", "Writer");
-            //}
-            //else
-            //{
-            //    return View();
-            //}
-            #endregion
-
+            return View(p);
+            
         }
+
+
+
+        //[HttpPost]
+
+        //public async Task<IActionResult> Index(Writer writer)
+        //{
+        //    #region Claims
+        //    Context c = new Context();
+        //    var result = c.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+        //    if (result != null)
+        //    {
+        //        var claims = new List<Claim>()
+        //        {
+        //            new Claim(ClaimTypes.Name,writer.WriterMail)
+        //        };
+        //        var useridentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+
+        //    #endregion
+        //    #region Session
+        //    //Context c = new Context();
+        //    //var result = c.Writers.FirstOrDefault(x => x.WriterMail == writer.WriterMail && x.WriterPassword == writer.WriterPassword);
+        //    //if (result != null)
+        //    //{
+        //    //    HttpContext.Session.SetString("username", writer.WriterMail);
+        //    //    return RedirectToAction("Index", "Writer");
+        //    //}
+        //    //else
+        //    //{
+        //    //    return View();
+        //    //}
+        //    #endregion
+
+        //}
 
 
     }
---
+++ CoreBlog/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreBlog.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı Adı Giriniz")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Şifre Giriniz")]
+        public string Password { get; set; }
+    }
+}
