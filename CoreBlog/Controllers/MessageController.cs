63ac58821b04ab011932b3d68d69e2c6833c0bb8
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-08-18 22:52:41 +0300

    messsage add

--- CoreBlog/Controllers/MessageController.cs
+++ CoreBlog/Controllers/MessageController.cs
@@ -1,8 +1,10 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace CoreBlog.Controllers
@@ -13,7 +15,6 @@ namespace CoreBlog.Controllers
         MessageManager mm = new MessageManager(new EfMessageRepository());
         Context c=new Context();
 
-        [AllowAnonymous]
         public IActionResult Inbox()
         {
             var username = User.Identity.Name;
@@ -24,11 +25,36 @@ namespace CoreBlog.Controllers
         }
 
 
-        [AllowAnonymous]
         public IActionResult MessageDetails(int id)
         {
             var result = mm.TGetById(id);
             return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SendMessage(Message p)
+        {
+
+            var username = User.Identity.Name;
+            var usermail= c.Users.Where(x => x.UserName == username).Select(x=>x.Email).FirstOrDefault();
+            var writerid=c.Writers.Where(c => c.WriterMail == usermail).Select(c => c.WriterId).FirstOrDefault();
+            
+            p.SenderId = writerid;
+            p.ReceiverId = 2;
+            p.MessageStatus = true;
+            p.MessageDate =Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            mm.TAdd(p);
+            return RedirectToAction("inbox", "Message");
+
+
+
         }
     }
 }
