4fc9fc86da60c2424cca630f32e6ec9c86fa1a45
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-08-20 13:16:24 +0300

    Admin Blog

--- CoreBlog/Areas/Admin/Controllers/MessageController.cs
+++ CoreBlog/Areas/Admin/Controllers/MessageController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace CoreBlog.Areas.Admin.Controllers
@@ -17,22 +19,57 @@ namespace CoreBlog.Areas.Admin.Controllers
             var username=User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
             var writerid=c.Writers.Where(x=>x.WriterMail == usermail).Select(x => x.WriterId).FirstOrDefault();
+            ViewBag.v2 = c.Messages.Where(x => x.ReceiverId == writerid).Count();
+            ViewBag.v1 = c.Messages.Where(x => x.SenderId == writerid).Count();
             var values = mm.GetInboxListByWriter(writerid);
             return View(values);
         }
 
         public IActionResult SendBox()
         {
+            
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
             var writerid = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterId).FirstOrDefault();
+            ViewBag.v1= c.Messages.Where(x => x.SenderId == writerid).Count();
+            ViewBag.v2 = c.Messages.Where(x => x.ReceiverId == writerid).Count();
             var values = mm.GetSendBoxListByWriter(writerid);
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult ComposeMail()
         {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
+            var writerid = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterId).FirstOrDefault();
+            ViewBag.v1 = c.Messages.Where(x => x.SenderId == writerid).Count();
+            ViewBag.v2 = c.Messages.Where(x => x.ReceiverId == writerid).Count();
             return View();
         }
+
+        [HttpPost]
+        public IActionResult ComposeMail(Message p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
+            var writerid = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterId).FirstOrDefault();
+
+            p.MessageStatus = true;
+            p.SenderId = writerid;
+            p.ReceiverId = 2;
+            p.MessageDate =Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            
+            
+            mm.TAdd(p);
+            return RedirectToAction("SendBox");
+
+        }
+
+
+
     }
+
+   
 }
